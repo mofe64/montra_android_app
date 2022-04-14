54ea84324516ec09863c9e625f404b551a2c48e9
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2022-04-14 16:49:32 +0100

    budget dao tests

---
+++ app/src/androidTest/java/com/nubari/montra/TestUtil.kt
@@ -0,0 +1,30 @@
+package com.nubari.montra
+
+import com.nubari.montra.data.local.models.Budget
+import com.nubari.montra.data.local.models.enums.BudgetType
+import java.math.BigDecimal
+import java.util.*
+
+object TestUtil {
+    fun generateMultipleBudgets(count: Int): List<Budget> {
+        val budgets: MutableList<Budget> = mutableListOf();
+        for (i in 1..count) {
+            val budget = Budget(
+                "budget$i",
+                BudgetType.CATEGORY,
+                shouldNotify = true,
+                100,
+                exceeded = false,
+                Date(),
+                BigDecimal(10000),
+                BigDecimal.ZERO,
+                "testCategoryId",
+                "Food",
+                "test123",
+                Date()
+            )
+            budgets.add(budget)
+        }
+        return budgets;
+    }
+}
--- app/src/androidTest/java/com/nubari/montra/data/local/dao/BudgetDaoTest.kt
+++ app/src/androidTest/java/com/nubari/montra/data/local/dao/BudgetDaoTest.kt
@@ -1,4 +1,146 @@
 package com.nubari.montra.data.local.dao
 
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.nubari.montra.TestUtil
+import com.nubari.montra.data.local.MontraDatabase
+import com.nubari.montra.data.local.models.Budget
+import com.nubari.montra.data.local.models.enums.BudgetType
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asExecutor
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.math.BigDecimal
+import java.util.*
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import timber.log.Timber
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
 class BudgetDaoTest {
+    private lateinit var db: MontraDatabase
+    private lateinit var budgetDao: BudgetDao
+    private val testDispatcher = TestCoroutineDispatcher()
+    private val testScope = TestCoroutineScope(testDispatcher)
+    private lateinit var budget: Budget
+    private lateinit var budget2: Budget
+    private val tag = "Budget_Dao_test"
+
+    @Before
+    fun setUp() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, MontraDatabase::class.java
+        )
+            .setTransactionExecutor(testDispatcher.asExecutor())
+            .setQueryExecutor(testDispatcher.asExecutor())
+            .build()
+
+        budgetDao = db.budgetDao
+        val budgets = TestUtil.generateMultipleBudgets(2);
+        budget = budgets[0]
+        budget2 = budgets[1]
+    }
+
+    @After
+    fun tearDown() {
+        db.clearAllTables()
+        db.close()
+    }
+
+    @Test
+    fun testCreateBudget() {
+        testScope.runBlockingTest {
+            budgetDao.createBudget(budget);
+            val savedBudget = budgetDao.getBudget(budget.id)
+            assertThat(savedBudget).isNotNull()
+            assertThat(savedBudget!!.id).isEqualTo(budget.id)
+        }
+    }
+
+    @Test
+    fun testSaveBudgets() {
+        testScope.runBlockingTest {
+            budgetDao.saveBudgets(*(listOf(budget, budget2)).toTypedArray())
+
+            val budgetLists = budgetDao.getBudgets().take(1)
+                .toList()[0]
+            Timber.tag(tag).d(budgetLists.toString())
+            assertThat(budgetLists.size).isEqualTo(2)
+            assertThat(budgetLists[0].id).isEqualTo(budget.id)
+        }
+    }
+
+    @Test
+    fun testDeleteBudget() {
+        testScope.runBlockingTest {
+            budgetDao.saveBudgets(*(listOf(budget, budget2)).toTypedArray())
+
+            var budgetLists = budgetDao.getBudgets().take(1)
+                .toList()[0]
+            Timber.tag(tag).d(budgetLists.toString())
+            assertThat(budgetLists.size).isEqualTo(2)
+            assertThat(budgetLists[0].id).isEqualTo(budget.id)
+
+            budgetDao.deleteBudget(budget)
+            budgetLists = budgetDao.getBudgets().take(1)
+                .toList()[0]
+            assertThat(budgetLists.size).isEqualTo(1)
+            assertThat(budgetLists[0].id).isEqualTo(budget2.id)
+
+        }
+    }
+
+    @Test
+    fun testGetBudgets(){
+        testScope.runBlockingTest {
+            budgetDao.saveBudgets(*(listOf(budget, budget2)).toTypedArray())
+
+            val budgetLists = budgetDao.getBudgets().take(1)
+                .toList()[0]
+            assertThat(budgetLists.size).isEqualTo(2)
+        }
+    }
+
+    @Test
+    fun testRetrieveBudgetList(){
+        testScope.runBlockingTest {
+            budgetDao.saveBudgets(*(listOf(budget, budget2)).toTypedArray())
+            val budgets = budgetDao.retrieveBudgetList()
+            assertThat(budgets.size).isEqualTo(2)
+        }
+    }
+
+    @Test
+    fun testGetBudget(){
+        testScope.runBlockingTest {
+            budgetDao.saveBudgets(*(listOf(budget, budget2)).toTypedArray())
+            val savedBudget = budgetDao.getBudget(budget2.id)
+            assertThat(savedBudget).isNotNull()
+            assertThat(savedBudget!!.id).isEqualTo(budget2.id)
+        }
+    }
+
+    @Test
+    fun testUpdateBudgetSpend(){
+        testScope.runBlockingTest {
+            val oldSpend = budget.spend
+            budgetDao.saveBudgets(*(listOf(budget, budget2)).toTypedArray())
+            budgetDao.updateBudgetSpend(BigDecimal.valueOf(10000), true, budget.id)
+            val updatedBudget = budgetDao.getBudget(budget.id)
+            assertThat(oldSpend).isNotEqualTo(updatedBudget!!.spend)
+            assertThat(updatedBudget!!.exceeded).isTrue()
+        }
+    }
 }
